6d1cc8ded9f92e1d04a271999937fdf6832cf1cd
김영호 <ksemfn@gmail.com>
2022-04-26 09:01:20 +0900

    리플렉션 수정.

--- richpath/src/main/java/com/richpath/pathparser/PathParser.kt
+++ richpath/src/main/java/com/richpath/pathparser/PathParser.kt
@@ -1,7 +1,8 @@
 package com.richpath.pathparser
 
+
 import android.graphics.Path
-import android.os.Build
+import androidx.core.graphics.PathParser
 
 object PathParser {
 
@@ -10,11 +11,7 @@ object PathParser {
      * @return the generated Path object.
      */
     fun createPathFromPathData(pathData: String?): Path {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            PathParserCompatApi21.createPathFromPathData(pathData) ?: Path()
-        } else {
-            PathParserCompat.createPathFromPathData(pathData)
-        }
+        return PathParser.createPathFromPathData(pathData) ?: Path()
     }
 
 }
